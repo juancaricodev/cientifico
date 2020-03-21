df9b3e2f7afbe43689ded677ac8d374004ca17d8
Juan Camilo Rico <juancarico92@gmail.com>
2020-03-21 14:38:19 -0500

    feat(structure): project structure defined

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node.modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin([
+      {
+        inject: true,
+        template: './public/index.html',
+        filename: './index.html'
+      }
+    ])
+  ]
+}
