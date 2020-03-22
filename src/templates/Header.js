39a5f1243f439f6b47739afd60776d260d066b8d
Juan Camilo Rico <juancarico92@gmail.com>
2020-03-21 20:26:54 -0500

    feat(pages): pages and templates added

---
+++ src/templates/Header.js
@@ -0,0 +1,19 @@
+const Header = () => {
+  const view = `
+    <div class="header-main">
+      <div class='header-logo">
+        <a href="/">
+          100tifi.co
+        </a>
+      </div>
+      <div class="header-nav">
+        <a href="#/about/">
+          About
+        </a>
+      </div>
+    </div>
+  `;
+  return view;
+};
+
+export default Header;
