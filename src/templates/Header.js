5e7eb08fe70fd329c5b5487f53c819331ff2f727
Juan Camilo Rico <juancarico92@gmail.com>
2020-03-21 22:08:47 -0500

    feat(router): router added

--- src/templates/Header.js
+++ src/templates/Header.js
@@ -1,10 +1,12 @@
 const Header = () => {
   const view = `
     <div class="header-main">
-      <div class='header-logo">
-        <a href="/">
-          100tifi.co
-        </a>
+      <div class="header-logo">
+        <h1>
+          <a href="/">
+            100tifi.co
+          </a>
+        </h1>
       </div>
       <div class="header-nav">
         <a href="#/about/">
